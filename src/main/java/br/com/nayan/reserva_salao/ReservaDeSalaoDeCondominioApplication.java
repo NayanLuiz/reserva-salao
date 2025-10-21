d034b8f2c4996e765f75362059ec25d0ef05e596
Nayan <nayanlima27@gmail.com>
2025-10-21 17:51:42 -0300

    Projeto inicial

---
+++ src/main/java/br/com/nayan/reserva_salao/ReservaDeSalaoDeCondominioApplication.java
@@ -0,0 +1,13 @@
+package br.com.nayan.reserva_salao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservaDeSalaoDeCondominioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservaDeSalaoDeCondominioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/nayan/reserva_salao/ReservaDeSalaoDeCondominioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nayan.reserva_salao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservaDeSalaoDeCondominioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
